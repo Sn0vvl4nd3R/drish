ae86451f201981d94dabc128cc0f4c77801e6187
Nikita Vasilev <ioganurij953@gmail.com>
2026-03-22 15:34:51 +0800

    added some cool features

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@
 #include <string>
 #include <vector>
 
-const size_t offset = 0xcbf29ce484222325;
-const size_t prime = 0x100000001b3;
+const uint64_t offset = 0xcbf29ce484222325;
+const uint64_t prime = 0x100000001b3;
 
-const int height = 9;
-const int width = 7;
+int height = 0;
+int width = 0;
 
-uint64_t fnv(std::string str) {
+uint64_t fnv(const std::string& str) {
   uint64_t hash = offset;
   for (const char& byte : str) {
     hash ^= byte;
@@ -21,17 +21,17 @@ uint64_t fnv(std::string str) {
 
 void PrintSymLine(void) {
   std::cout << '+';
-  for (size_t i = 0; i < width; ++i) std::cout << '-';
+  for (int i = 0; i < width; ++i) std::cout << '-';
   std::cout << "+\n";
 }
 
-void PrintMatrix(const std::vector<std::vector<int>>& matrix, size_t start_x,
-                 size_t start_y, size_t end_x, size_t end_y) {
+void PrintMatrix(const std::vector<std::vector<int>>& matrix, int start_x,
+                 int start_y, int end_x, int end_y) {
   const std::string symbols = " .o+=*BOX@%&#/^";
   PrintSymLine();
-  for (size_t y = 0; y < height; ++y) {
+  for (int y = 0; y < height; ++y) {
     std::cout << '|';
-    for (size_t x = 0; x < width; ++x) {
+    for (int x = 0; x < width; ++x) {
       if (x == start_x && y == start_y)
         std::cout << "S";
       else if (x == end_x && y == end_y)
@@ -47,48 +47,60 @@ void PrintMatrix(const std::vector<std::vector<int>>& matrix, size_t start_x,
 }
 
 int main(void) {
+  std::cout << "Enter the height & width:\n";
+  std::cin >> height >> width;
+
   std::vector<std::vector<int>> vec(height, std::vector<int>(width, 0));
   int x = width / 2;
   int y = height / 2;
 
-  std::string str("Hello, World!");
-  uint64_t hash = fnv(str);
+  std::cout << "Enter your string:\n";
+  std::string str;
+  std::getline(std::cin >> std::ws, str);
 
-  for (size_t i = 0; i < 32; ++i) {
-    int bit = hash & 3;
-    hash >>= 2;
+  int total_steps = width * height;
+  int steps_done = 0;
+  int iter = 0;
+  while (steps_done < total_steps) {
+    uint64_t hash = fnv(str + std::to_string(iter));
+    iter++;
+    for (size_t i = 0; i < 32 && steps_done < total_steps; ++i) {
+      int bit = hash & 3;
+      hash >>= 2;
+      steps_done++;
 
-    switch (bit) {
-      case (0):
-        --x;
-        --y;
-        break;
-      case (1):
+      switch (bit) {
+        case (0):
+          --x;
+          --y;
+          break;
+        case (1):
+          ++x;
+          --y;
+          break;
+        case (2):
+          --x;
+          ++y;
+          break;
+        case (3):
+          ++x;
+          ++y;
+          break;
+      }
+
+      if (x < 0) {
         ++x;
-        --y;
-        break;
-      case (2):
+      } else if (x > width - 1) {
         --x;
+      }
+      if (y < 0) {
         ++y;
-        break;
-      case (3):
-        ++x;
-        ++y;
-        break;
-    }
+      } else if (y > height - 1) {
+        --y;
+      }
 
-    if (x < 0) {
-      ++x;
-    } else if (x > width - 1) {
-      --x;
+      vec[y][x]++;
     }
-    if (y < 0) {
-      ++y;
-    } else if (y > height - 1) {
-      --y;
-    }
-
-    vec[y][x]++;
   }
   PrintMatrix(vec, width / 2, height / 2, x, y);
   return 0;
